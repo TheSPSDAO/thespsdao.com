676348e10bbd189087e80c9aa6b0e2c2ba5ddf0b
Stephen Moyer <smoyer@splinterlands.com>
2025-05-30 14:46:30 -0400

    Tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
+  integrations: [],
   vite: {
     plugins: [tailwindcss()],
   },
